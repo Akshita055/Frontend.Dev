62b1d84236bfa1369fd20467d4c423537d2d2def
Akshita YourName <you@example.com>
2025-11-19 00:13:44 +0530

    Clean start

--- JavaScript/Assignments/Js-operators/Q9/script.js
+++
@@ -1,9 +0,0 @@
-const result = [];
-
-for (let i = 1; i <= 30; i++) {
-  if (i % 3 === 0 && i % 5 === 0) result.push("FizzBuzz");
-  else if (i % 2 === 0) result.push("Even");
-  else result.push("Odd");
-}
-
-console.log(result);
--- JavaScript/Classroom/Assignment1.js
+++
@@ -1,3 +0,0 @@
-// date - 10 09 2025
-// Assignment 1 
-console.log(Math.random() * (100 - 1) + 1);
